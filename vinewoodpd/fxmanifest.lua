b29284699ec7d420b3f23a080e9dd6e03b940161
LeekowS <66480633+LeekowS@users.noreply.github.com>
2020-11-01 22:32:44 +0300

    Add files via upload

---
+++ vinewoodpd/fxmanifest.lua
@@ -0,0 +1,10 @@
+--------------------------
+--LEAK BY "MYCROFT LEAKS"
+--LEAK BY "MYCROFT LEAKS"
+--LEAK BY "MYCROFT LEAKS"
+--------------------------
+fx_version 'bodacious'
+game  'gta5'
+this_is_a_map 'yes'
+
+client_script 'client.lua'
